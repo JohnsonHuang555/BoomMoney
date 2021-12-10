b445ad364a2fed45240bbccc3edad52b419ea2e7
JohnsonHuang0707 <imchung0707@gmail.com>
2021-12-10 21:04:42 +0800

    first commit
    開工大吉

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] private Color baseColor, offsetColor;
+    [SerializeField] private SpriteRenderer render;
+    [SerializeField] private GameObject highlight;
+
+    public void Init(bool isOffset)
+    {
+        render.color = isOffset ? offsetColor : baseColor;
+    }
+
+    private void OnMouseEnter()
+    {
+        highlight.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        highlight.SetActive(false);
+    }
+}
