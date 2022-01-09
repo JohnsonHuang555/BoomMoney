651347a2ef26e66c0d68c4b292355fddb0597cb4
JohnsonHuang0707 <imchung0707@gmail.com>
2022-01-09 16:01:37 +0800

    移動資料夾

--- Assets/_Scripts/UIControllers/MainMenuController.cs
+++ Assets/_Scripts/UIControllers/MainMenuController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
 
-public class UIController : MonoBehaviour
+public class MainMenuController : MonoBehaviour
 {
     public Button startButton;
 
